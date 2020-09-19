0ad3f952003c208ad3d378e97e5f2d71b4cf3502
ajelwan <andrew.jelwan@gmail.com>
2020-09-20 01:57:15 +1000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import { VueLoaderPlugin } from "vue-loader";
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    alias: {
+      "bootstrap-vue$": "bootstrap-vue/src/index.js"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules\/(?!bootstrap-vue\/src\/)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()]
+};
