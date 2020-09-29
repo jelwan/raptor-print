c23e00a93bb7c352391f514d1fa673334fe6f01e
jelwan <andrew.jelwan@gmail.com>
2020-09-29 16:16:42 +1000

    npm lint

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 
-import './custom.scss'
+import "./custom.scss";
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   router,
